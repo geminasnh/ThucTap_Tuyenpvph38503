6fb53a4b33f0e5c88ca69d3899bc4158ba7bfa12
An <anpx123@gmail.com>
2024-07-13 23:22:48 +0700

    style(admin/product): improve layout of product page

--- resources/views/admins/sanphams/index.blade.php
+++ resources/views/admins/sanphams/index.blade.php
@@ -22,50 +22,107 @@
         @endif
     </script>
 
-    <div class="table-responsive">
-        <table class="table striped-table mb-0">
-            <thead>
-            <tr>
-                <th scope="col">Sản phẩm</th>
-                <th scope="col">SL</th>
-                <th scope="col">Giá</th>
-                <th scope="col">Giá Sale</th>
-                <th scope="col">Ngày nhập</th>
-                <th scope="col">Mô tả</th>
-                <th scope="col">Thao tác</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($listProduct as $index => $sp)
+    <div class="card">
+        <div class="card-header d-flex flex-wrap align-items-center justify-content-between gap-3">
+            <div class="d-flex flex-wrap align-items-center gap-3">
+                <div class="d-flex align-items-center gap-2">
+                    <span>Hiển thị</span>
+                    <select class="form-select form-select-sm w-auto">
+                        <option>10</option>
+                        <option>15</option>
+                        <option>20</option>
+                    </select>
+                </div>
+                <div class="icon-field">
+                    <input type="text" name="#0" class="form-control form-control-sm w-auto" placeholder="Tìm kiếm">
+                    <span class="icon">
+              <iconify-icon icon="ion:search-outline"></iconify-icon>
+            </span>
+                </div>
+            </div>
+            <div class="d-flex flex-wrap align-items-center gap-3">
+                <select class="form-select form-select-sm w-auto">
+                    <option selected>Chọn danh mục</option>
+                    <option value="1">Hoa quả</option>
+                    <option value="2">Rau</option>
+                </select>
+                <a href="{{route('sanpham.create')}}" class="btn btn-sm btn-primary-600"><i class="ri-add-line"></i>
+                    Thêm sản phẩm</a>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table bordered-table mb-0">
+                <thead>
                 <tr>
-                    <td>
-                        <div class="d-flex align-items-center">
-                            <img src="#" alt="" class="flex-shrink-0 me-12 radius-8 me-12">
-                            <div class="flex-grow-1">
-                                <h6 class="text-md mb-0 fw-normal">{{$sp->ten_san_pham}}</h6>
-                                <span class="text-sm text-secondary-light fw-normal">{{$sp->danh_muc_id}}</span>
-                            </div>
-                        </div>
-                    </td>
-                    <td>{{$sp->so_luong}}</td>
-                    <td>{{$sp->gia}}</td>
-                    <td>{{$sp->gia_khuyen_mai}}</td>
-                    <td>{{$sp->ngay_nhap}}</td>
-                    <td style="width: 527px">{{$sp->mo_ta}}</td>
-                    <td>
-                        <a href="#"
-                           class="w-32-px h-32-px bg-success-focus text-success-main rounded-circle d-inline-flex align-items-center justify-content-center">
-                            <iconify-icon icon="lucide:edit"></iconify-icon>
-                        </a>
-                        <a href="#"
-                           class="w-32-px h-32-px bg-danger-focus text-danger-main rounded-circle d-inline-flex align-items-center justify-content-center">
-                            <iconify-icon icon="mingcute:delete-2-line"></iconify-icon>
-                        </a>
-                    </td>
+                    <th scope="col">Sản phẩm</th>
+                    <th scope="col">SL</th>
+                    <th scope="col">Giá</th>
+                    <th scope="col">Giá Sale</th>
+                    <th scope="col">Ngày nhập</th>
+                    <th scope="col">Mô tả</th>
+                    <th scope="col">Thao tác</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach($listProduct as $index => $sp)
+                    <tr>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <img src="#" alt="" class="flex-shrink-0 me-12 radius-8 me-12">
+                                <div class="flex-grow-1">
+                                    <h6 class="text-md mb-0 fw-normal">{{$sp->ten_san_pham}}</h6>
+                                    <span class="text-sm text-secondary-light fw-normal">{{$sp->danh_muc_id}}</span>
+                                </div>
+                            </div>
+                        </td>
+                        <td>{{$sp->so_luong}}</td>
+                        <td>{{$sp->gia}}</td>
+                        <td>{{$sp->gia_khuyen_mai}}</td>
+                        <td>{{$sp->ngay_nhap}}</td>
+                        <td>{{$sp->mo_ta}}</td>
+                        <td>
+                            <a href="#"
+                               class="w-32-px h-32-px bg-success-focus text-success-main rounded-circle d-inline-flex align-items-center justify-content-center">
+                                <iconify-icon icon="lucide:edit"></iconify-icon>
+                            </a>
+                            <a href="#"
+                               class="w-32-px h-32-px bg-danger-focus text-danger-main rounded-circle d-inline-flex align-items-center justify-content-center">
+                                <iconify-icon icon="mingcute:delete-2-line"></iconify-icon>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+            <div class="d-flex flex-wrap align-items-center justify-content-between gap-2 mt-24">
+                <span>Hiển thị 1 tới 10 trên 12 trang</span>
+                <ul class="pagination d-flex flex-wrap align-items-center gap-2 justify-content-center">
+                    <li class="page-item">
+                        <a class="page-link text-secondary-light fw-medium radius-4 border-0 px-10 py-10 d-flex align-items-center justify-content-center h-32-px w-32-px bg-base"
+                           href="javascript:void(0)">
+                            <iconify-icon icon="ep:d-arrow-left" class="text-xl"></iconify-icon>
+                        </a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link bg-primary-600 text-white fw-medium radius-4 border-0 px-10 py-10 d-flex align-items-center justify-content-center h-32-px w-32-px"
+                           href="javascript:void(0)">1</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link bg-primary-50 text-secondary-light fw-medium radius-4 border-0 px-10 py-10 d-flex align-items-center justify-content-center h-32-px w-32-px"
+                           href="javascript:void(0)">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link bg-primary-50 text-secondary-light fw-medium radius-4 border-0 px-10 py-10 d-flex align-items-center justify-content-center h-32-px w-32-px"
+                           href="javascript:void(0)">3</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link text-secondary-light fw-medium radius-4 border-0 px-10 py-10 d-flex align-items-center justify-content-center h-32-px w-32-px bg-base"
+                           href="javascript:void(0)">
+                            <iconify-icon icon="ep:d-arrow-right" class="text-xl"></iconify-icon>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-
 @endsection
